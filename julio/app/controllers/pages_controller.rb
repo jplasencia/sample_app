be3f429a834f654f7c25fc74594fbc4e5f014413
Julio Plasencia <julioplasencia@gmail.com>
2012-03-04 00:40:35 +0100

    Done with static pages

--- julio/app/controllers/pages_controller.rb
+++ julio/app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Home In the House or ME"
   end
 
   def contact
+    @title = "Contact"
   end
 
+  def about
+    @title = "About"
+  end
 end
--- julio/config/routes.rb
+++ julio/config/routes.rb
@@ -2,6 +2,8 @@ Julio::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+   get "pages/about"
 
   resources :microposts
 

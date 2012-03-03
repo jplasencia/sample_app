33da9c2896bede2b5877ab54777b3be82303423b
Julio Plasencia <julioplasencia@gmail.com>
2012-03-03 23:42:04 +0100

    Added a pages controller

---
+++ julio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Julio::Application.config.secret_token = 'e2b024e7c745b1282cd01aacc453308de7551416ac1e0cc7a0e430cff94ba6ef508ab8dd2291ad6603e82e963d31169e11fe0acc01b927aded56ac4992a79151'
